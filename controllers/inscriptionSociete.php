f8ef1111ef1c567d27da711a15df08fcab852ee2
FIRST_tristan LAST_richard <tristan3755@gmail.com>
2020-10-12 14:15:32 +0200

    projet

--- controllers/inscriptionSociete.php
+++ controllers/inscriptionSociete.php
@@ -10,10 +10,12 @@ if(!empty($_POST)){
 
 connectSociete($_POST['nom'],$_POST['adresse'],$_POST['tel'],password_hash($_POST['password'].'nader',PASSWORD_BCRYPT));
 
-header('location:../controllers/pageConnecteeSociete.php');
-exit;
+/*header('location:../controllers/pageConnecteeSociete.php');
+exit;*/
 
 }
+
+
 
 
 
---
+++ models/BDDinscriptionSociete.php
@@ -0,0 +1,31 @@
+<?php
+
+
+define('DATABASE_DSN','mysql:host=localhost;dbname=monprojet;charset=utf8');
+define('DATABASE_USERNAME','root');
+define('DATABASE_PASSWORD', '');
+
+function connectSociete(string $nom,$adresse,$tel,$password){
+
+
+    $dbh = new PDO
+	(
+		DATABASE_DSN,
+		DATABASE_USERNAME,
+		DATABASE_PASSWORD,
+		[
+			PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+			PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+		]
+	);
+$query='INSERT INTO societes(nom,adresse,telephone,hashedPassword) VALUES (:nom,:adresse,:tel,:hashedPassword)';
+$sth=$dbh ->prepare($query);
+$sth->bindValue(':nom',$nom,PDO::PARAM_STR);
+$sth->bindValue(':adresse',$adresse,PDO::PARAM_STR);
+$sth->bindValue(':tel',$tel,PDO::PARAM_STR);
+$sth->bindValue(':hashedPassword',$password,PDO::PARAM_STR);
+$sth->execute();
+
+};
+
+?>
