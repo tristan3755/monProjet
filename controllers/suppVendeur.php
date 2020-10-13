d93b73dbd6314abb0bb2d07cf5945a1001569570
FIRST_tristan LAST_richard <tristan3755@gmail.com>
2020-10-13 21:21:12 +0200

    projet

---
+++ controllers/suppVendeur.php
@@ -0,0 +1,10 @@
+<?php
+
+include "../models/BDDsuppVendeur.php";
+
+$idSupp=supp($_GET['id']);
+
+var_dump($_GET)
+
+
+?>
---
+++ models/BDDsuppVendeur.php
@@ -0,0 +1,40 @@
+<?php
+
+session_start();
+if(!array_key_exists('societeId',$_SESSION)){
+	header('location:../controllers/connexion.php');
+    exit;
+}
+
+
+define('DATABASE_DSN','mysql:host=localhost;dbname=monprojet;charset=utf8');
+define('DATABASE_USERNAME', 'root');
+define('DATABASE_PASSWORD', '');
+
+
+
+
+
+function supp($id){
+
+    $dbh = new PDO
+(
+    DATABASE_DSN,
+    DATABASE_USERNAME,
+    DATABASE_PASSWORD,
+    [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]
+);
+
+
+    $query='DELETE FROM vendeur WHERE id_vendeur = :id';
+    $sth=$dbh ->prepare($query);
+    $sth->bindValue(':id',$id,PDO::PARAM_STR);
+    $sth->execute();
+
+
+
+
+}
