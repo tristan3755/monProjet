da651a504405b4ea85c281618ecce4fb8ef4c25f
FIRST_tristan LAST_richard <tristan3755@gmail.com>
2020-10-16 14:55:40 +0200

    projet

--- models/BDDmodifClient.php
+++ models/BDDmodifClient.php
@@ -15,7 +15,7 @@ define('DATABASE_PASSWORD','');
 
 
 
-function modif($id,$nom,$prenom,$tel,$ref,$email,){
+function modif($id,$nom,$prenom,$tel,$ref,$email,$adresse,$ville,$codePostal){
 
     $dbh = new PDO
 (
@@ -29,15 +29,20 @@ function modif($id,$nom,$prenom,$tel,$ref,$email,){
 );
 
 
-    $query='UPDATE clients SET  WHERE id_clients = :id';
+    $query='UPDATE clients SET nom=:nom,prenom=:prenom,telephone=:telephone,ref_client=:ref,email=:email,adresse=:adresse,ville=:ville,CodePostal=:code WHERE id_clients = :id';
     $sth=$dbh ->prepare($query);
     $sth->bindValue(':nom',$nom,PDO::PARAM_STR);
     $sth->bindValue(':prenom',$prenom,PDO::PARAM_STR);
+    $sth->bindValue(':telephone',$tel,PDO::PARAM_STR);
     $sth->bindValue(':ref',$ref,PDO::PARAM_STR);
     $sth->bindValue(':email',$email,PDO::PARAM_STR);
-    $sth->bindValue(':telephone',$tel,PDO::PARAM_STR);
-    $sth->bindValue(':hashedPassword',$password,PDO::PARAM_STR);
+    $sth->bindValue(':adresse',$adresse,PDO::PARAM_STR);
+    $sth->bindValue(':ville',$ville,PDO::PARAM_STR);
+    $sth->bindValue(':codePostal',$codePostal,PDO::PARAM_STR);
     $sth->bindValue(':id',$id,PDO::PARAM_INT);
     $sth->execute();
 
 }
+
+
+?>
