e05c8288cf1f97c5a7964417543fea33e08cbafe
FIRST_tristan LAST_richard <tristan3755@gmail.com>
2020-10-26 09:50:52 +0100

    projet

--- controllers/listeFactureClient.php
+++ controllers/listeFactureClient.php
@@ -6,6 +6,8 @@ require "../models/BDDlisteFactureClient.php";
 $liste=recupFacture($_GET['id']);
 
 
+
+
 var_dump($_GET,$_SESSION,$liste);
 
 include ('../views/listeFactureClient.phtml');
--- controllers/modifListeFacture.php
+++ controllers/modifListeFacture.php
@@ -4,6 +4,11 @@ require "../models/BDDmodifList.php";
 
 $client=infoClient($_GET['id']);
 
+/*
+modifListeFacture($_GET['id'],$_POST['datePaiement'],$_POST['commentaire'],$_POST['adresseLivraison'],$_POST['villeLivraison'],$_POST['CodeLivraison'],
+$_POST['adresseFacturation'],$_POST['villeFacturation'],$_POST['codeFacturation'],$_POST['statut']);
+*/
+
 var_dump($client);
 
 include "../views/modifList.phtml";
