ba83d39f306b51159df69d5ca2db29c49feeb44f
FIRST_tristan LAST_richard <tristan3755@gmail.com>
2020-10-26 11:14:25 +0100

    projet

--- controllers/modifListeFacture.php
+++ controllers/modifListeFacture.php
@@ -4,10 +4,10 @@ require "../models/BDDmodifList.php";
 
 $client=infoClient($_GET['id']);
 
-/*
+
 modifListeFacture($_GET['id'],$_POST['datePaiement'],$_POST['commentaire'],$_POST['adresseLivraison'],$_POST['villeLivraison'],$_POST['CodeLivraison'],
 $_POST['adresseFacturation'],$_POST['villeFacturation'],$_POST['codeFacturation'],$_POST['statut']);
-*/
+
 
 var_dump($client);
 
--- models/BDDmodifList.php
+++ models/BDDmodifList.php
@@ -43,4 +43,48 @@ return $client;
 }
 
 
+function modifListeFacture($id,$datePaiement,$comm,$adressLiv,$villeLiv,$codeLiv,$adressFact,$villeFact,$codeFact,$statut){
+
+    $dbh = new PDO
+(
+    DATABASE_DSN,
+    DATABASE_USERNAME,
+    DATABASE_PASSWORD,
+    [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]
+);
+
+
+
+$query='UPDATE facture SET date_paiement=:datePaie,commentaire=:comm,adresse_livraison=:adressLiv,adresse_facturation:adressFact
+statut=:statut,ville_livraison=:villeLiv,codePostal_livraison:codeLiv,ville_facturation=:villeFact,codePostal_facturation=:codeFact WHERE :id=id_facture';
+
+
+$sth=$dbh->prepare($query);
+$sth->bindValue(':id',$id,PDO::PARAM_INT);
+$sth->bindValue(':datePaie',$datePaiement,PDO::PARAM_STR);
+$sth->bindValue(':comm',$comm,PDO::PARAM_STR);
+$sth->bindValue(':adressLiv',$adressLiv,PDO::PARAM_STR);
+$sth->bindValue(':villeLiv',$villeLiv,PDO::PARAM_STR);
+$sth->bindValue(':codeLiv',$codeLiv,PDO::PARAM_STR);
+$sth->bindValue(':adressFact',$adressFact,PDO::PARAM_STR);
+$sth->bindValue(':villeFact',$villeFact,PDO::PARAM_STR);
+$sth->bindValue(':codeFact',$codeFact,PDO::PARAM_STR);
+$sth->bindValue(':statut',$statut,PDO::PARAM_STR);
+
+
+$sth->execute();
+
+
+
+
+
+
+
+
+}
+
+
 ?>
