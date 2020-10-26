6ea1b58f184e1d8c27188edc739be54146900d5c
FIRST_tristan LAST_richard <tristan3755@gmail.com>
2020-10-26 14:34:35 +0100

    projet

---
+++ controllers/ajoutProduitFacture.php
@@ -0,0 +1,14 @@
+<?php
+
+
+// require "../models/BDDajoutProduitFacture.php";
+
+// $tabObjet=recupInfo($_GET['id']);
+
+var_dump($_GET);
+
+include "../views/ajoutProduitFacture.phtml";
+
+
+
+?>
---
+++ models/BDDajoutProduitFacture.php
@@ -0,0 +1,44 @@
+<?php
+
+session_start();
+if(!array_key_exists('vendeurId',$_SESSION)){
+
+	header('location:../controllers/connexion.php');
+    exit;
+};
+
+define('DATABASE_DSN','mysql:host=localhost;dbname=monprojet;charset=utf8');
+define('DATABASE_USERNAME', 'root');
+define('DATABASE_PASSWORD', '');
+
+
+function recupInfo($idFact){
+
+
+    $dbh = new PDO
+(
+    DATABASE_DSN,
+    DATABASE_USERNAME,
+    DATABASE_PASSWORD,
+    [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]
+);
+
+
+$query='SELECT * FROM '
+
+
+
+};
+
+
+
+
+
+
+
+
+
+?>
