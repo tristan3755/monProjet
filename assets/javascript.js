e54a33b4f7860f562323eeb9e7aa07b9a3065f67
FIRST_tristan LAST_richard <tristan3755@gmail.com>
2020-12-28 21:46:24 +0100

    projet

---
+++ assets/javascript.js
@@ -0,0 +1,13 @@
+window.addEventListener('load',(event)=>{
+
+header()
+
+})
+
+
+function header(){
+    let monHeader=document.querySelector('header')
+    monHeader.style.height="80px"
+    monHeader.style.transition="2s"
+    monHeader.style.opacity="1"
+}
