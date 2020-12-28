4475e8c42f7ccb30810e7f056bad6310688ff95b
FIRST_tristan LAST_richard <tristan3755@gmail.com>
2020-12-28 17:41:00 +0100

    projet

--- controllers/editionFacture.php
+++ controllers/editionFacture.php
@@ -1,13 +1,13 @@
 <?php
 
-var_dump($_GET);
+// var_dump($_GET);
 include "../models/BDDeditionFacture.php";
 
 $recupEdition=recupEdition($_GET["id"]);
 $recupEditionSociete=recupEditionSociete($recupEdition[0]["id_societes"]);
 $recupEditionClient=recupEditionClient($recupEdition[0]["id_clients"]);
 
-var_dump($recupEdition,$recupEditionSociete,$recupEditionClient);
+// var_dump($recupEdition,$recupEditionSociete,$recupEditionClient);
 
 require "../views/editionFacture.phtml";
 
