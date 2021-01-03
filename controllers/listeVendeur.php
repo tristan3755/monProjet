4118072b3033e6b93fc0203e57ae11922437c4cd
FIRST_tristan LAST_richard <tristan3755@gmail.com>
2021-01-03 15:59:21 +0100

    projet

--- controllers/listeVendeur.php
+++ controllers/listeVendeur.php
@@ -5,7 +5,7 @@
 require "../models/BDDlisteVendeur.php";
 
 $listeVendeur=vendeurs($_SESSION['societeId']);
-var_dump($listeVendeur);
+// var_dump($listeVendeur);
 
 include "../views/listeVendeur.phtml";
 
