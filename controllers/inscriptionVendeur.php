b9bce2b1bc854c08fbd989517cf3fbe8c5564afb
FIRST_tristan LAST_richard <tristan3755@gmail.com>
2021-01-14 11:54:44 +0100

    projet

--- controllers/inscriptionVendeur.php
+++ controllers/inscriptionVendeur.php
@@ -2,20 +2,18 @@
 
 require "../models/BDDinscriptionVendeur.php";
 
+var_dump($_SESSION);
+
 
 if(!empty($_POST)){
 
-    // var_dump($_POST);
+     
 
-    inscriptionVendeur($_POST['nom'],$_POST['prenom'],$_POST['ref'],$_POST['email'],$_POST['telephone'],password_hash($_POST['password'].'nader',PASSWORD_BCRYPT),($_SESSION['societeId']));
-    
-   
-    
+    inscriptionVendeur($_POST['nom'],$_POST['prenom'],$_POST['ref'],$_POST['email'],$_POST['telephone']
+    ,password_hash($_POST['password'].'nader',PASSWORD_BCRYPT),($_SESSION['societeId']));
+       
 }
     
-
-
-
 include "../views/inscriptionVendeur.phtml";
 
 
