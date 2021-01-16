4ed6ea357be9fbdf504ed3de895a78d4fb5dc8f6
FIRST_tristan LAST_richard <tristan3755@gmail.com>
2021-01-16 15:46:56 +0100

    projet

--- controllers/inscriptionSociete.php
+++ controllers/inscriptionSociete.php
@@ -5,13 +5,13 @@ include "../models/BDDinscriptionSociete.php";
 
 
 
-if(!empty($_POST)){
+if(!empty($_POST['nom']) AND !empty($_POST['adresse']) AND !empty($_POST['tel'])AND !empty($_POST['password'])){
 
 
 connectSociete($_POST['nom'],$_POST['adresse'],$_POST['tel'],password_hash($_POST['password'].'nader',PASSWORD_BCRYPT));
 
-/*header('location:../controllers/pageConnecteeSociete.php');
-exit;*/
+header('location:../controllers/pageConnecteeSociete.php');
+exit;
 
 }
 
