5f98770e60f20e37d044b4ffdd8d5ead92bda15c
FIRST_tristan LAST_richard <tristan3755@gmail.com>
2021-01-17 19:54:31 +0100

    projet

--- controllers/listeFactureClient.php
+++ controllers/listeFactureClient.php
@@ -5,12 +5,13 @@ require "../models/BDDlisteFactureClient.php";
 
 $liste=recupFacture($_GET['id']);
 
+if(!empty($liste)){
 $objet=recupDetailFacture($liste['id_facture']);
+}
 
 
 
-
-// var_dump($_GET,$_SESSION,$liste,$objet);
+//var_dump($_GET,$_SESSION,$liste,$objet);
 
 include ('../views/listeFactureClient.phtml');
 
